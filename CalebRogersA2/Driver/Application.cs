53e1a4759abd318f0038321d67503f37b2aff046
Caleb Rogers <Caleb.rogers98@icloud.com>
2026-08-26 15:49:47 -0400

    Placeholder Added

--- CalebRogersA2/Driver/Application.cs
+++ CalebRogersA2/Driver/Application.cs
@@ -4,5 +4,7 @@ public class Application
 {
     public void Run()
     {
+        Console.WriteLine("Caleb Rogers Assignment 2");
+        // This is a place holder for testing purposes only.
     }
 }
