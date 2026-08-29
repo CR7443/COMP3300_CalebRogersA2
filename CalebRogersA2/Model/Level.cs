be48b5c9de1206f2968033702773eb134ff7cf34
Caleb Rogers <Caleb.rogers98@icloud.com>
2026-08-28 20:14:58 -0400

    Completed Driver Class

--- CalebRogersA2/Model/Level.cs
+++ CalebRogersA2/Model/Level.cs
@@ -2,10 +2,14 @@ namespace CalebRogersA2.Model;
 
 public class Level
 {
-    public Level()
+    public Level() : this(string.Empty)
+    {
+    }
+
+    public Level(string levelName)
     {
         RunAttempts = new List<Attempt>();
-        LevelName = string.Empty;
+        LevelName = levelName;
     }
 
     public List<Attempt> RunAttempts { get; }
@@ -13,4 +17,19 @@ public class Level
 
     public int RunCount => RunAttempts.Count;
     public int RecordRunCount => RunAttempts.Count(attempt => attempt.IsTimeBelowRecord);
+
+    public void AddLevel(Attempt attempt)
+    {
+        RunAttempts.Add(attempt);
+    }
+
+    public void AddLevel(string? firstName, string? lastName, int score, decimal time)
+    {
+        AddLevel(new Attempt(firstName, lastName, time, score));
+    }
+
+    public override string ToString()
+    {
+        return $"Level: {LevelName} – Total Attempts: {RunCount}, Record Runs: {RecordRunCount}";
+    }
 }
