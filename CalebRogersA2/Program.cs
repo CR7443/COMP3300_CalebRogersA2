f809d7bf39e74aaa3b52d74fb3f851edbe1a9271
Caleb Rogers <Caleb.rogers98@icloud.com>
2026-08-28 20:20:35 -0400

    Created Levels

--- CalebRogersA2/Program.cs
+++ CalebRogersA2/Program.cs
@@ -1,9 +1,17 @@
 ﻿using CalebRogersA2.Driver;
+using CalebRogersA2.Model;
 
 namespace CalebRogersA2;
 
 class Program
 {
+    private readonly Level _level;
+
+    public Program()
+    {
+        _level = new Level();
+    }
+
     static void Main(string[] args)
     {
         Application application = new Application();
