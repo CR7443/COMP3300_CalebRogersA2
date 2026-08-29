10d6a49facb003ffe1f06303a568b9f0b6096e8d
Caleb Rogers <Caleb.rogers98@icloud.com>
2026-08-28 20:51:01 -0400

    Removed Place Holder

--- CalebRogersA2/Driver/Application.cs
+++ CalebRogersA2/Driver/Application.cs
@@ -4,7 +4,7 @@ public class Application
 {
     public void Run()
     {
-        Console.WriteLine("Caleb Rogers Assignment 2");
+        // Console.WriteLine("Caleb Rogers Assignment 2");
         // This is a place holder for testing purposes only.
     }
 }
